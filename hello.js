abc0ccca97214c7f77a2edb8904e12b95879976b
Afnan Khattak <dev.reactx@gmail.com>
2025-03-29 16:45:36 +0500

    start stash from start

--- hello.js
+++ hello.js
@@ -2,24 +2,3 @@ console.log("hello");
 console.log("another beautiful console.log message::");
 console.log("a new feature has been implemented and shipped!");
 console.log("urgent fixed completed successfully!");
-const PORT = 3000;
-//start working on creating a express app
-
-const express = require("express");
-const app = express();
-
-
-app.get('/',(req,res)=>{
-    console.log('hello world!');
-})
-
-
-app.get('/users',(req,res)=>{
-    console.log('getting usersss!');
-})
-
-
-
-app.listen(PORT, () => {
-  console.log("Server is up and running!!");
-});
