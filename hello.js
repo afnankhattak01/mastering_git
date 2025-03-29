95c8e0536ed678f4c55f599f8802e35708c31e49
Afnan Khattak <dev.reactx@gmail.com>
2025-03-29 16:37:45 +0500

    add port fix

--- hello.js
+++ hello.js
@@ -1,21 +1,17 @@
-console.log("hello")
-console.log('another beautiful console.log message::');
+console.log("hello");
+console.log("another beautiful console.log message::");
 console.log("a new feature has been implemented and shipped!");
 console.log("urgent fixed completed successfully!");
-
+const PORT = 3000;
 //start working on creating a express app
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("hello world!");
+});
 
-app.get('/',(req,res)=>{
-    console.log('hello world!');
-})
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Server is up and running!!")
-})
+app.listen(PORT, () => {
+  console.log("Server is up and running!!");
+});
