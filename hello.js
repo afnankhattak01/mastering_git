798a8b4f838ef7f733ba302e2fa3d2c66078aba1
Afnan Khattak <dev.reactx@gmail.com>
2025-03-29 16:31:56 +0500

    stashed chages applied successfully

--- hello.js
+++ hello.js
@@ -2,3 +2,20 @@ console.log("hello")
 console.log('another beautiful console.log message::');
 console.log("a new feature has been implemented and shipped!");
 console.log("urgent fixed completed successfully!");
+
+//start working on creating a express app
+
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    console.log('hello world!');
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is up and running!!")
+})
