db1b7cb4046944e512ca6c65c9e6bdc715a29520
Afnan Khattak <dev.reactx@gmail.com>
2025-03-29 16:59:23 +0500

    new feature added, and error fixed

--- hello.js
+++ hello.js
@@ -2,3 +2,6 @@ console.log("hello");
 console.log("another beautiful console.log message::");
 console.log("a new feature has been implemented and shipped!");
 console.log("new error has been removed !!")
+
+
+const PORT = 5000;
